b343544a1fd47941dfdc669ef1ba5eaa4986e07a
Amir Ranjbr <mr.am.amir.ir@gmail.com>
2024-03-11 19:58:48 +0330

    refactor: remove // transition and add reveal

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,8 @@ const elements = document.querySelectorAll(".main__projects");
 const mainAbout = document.querySelector(".main-bottom-wrapper");
 const footerTop = document.querySelector(".footer-top");
 const menuWrapper = document.querySelector(".menu__wrapper");
+const allSection = document.querySelectorAll(".main__projects");
+const mainTop = document.querySelector(".main-top");
 // Scroll Section
 // Scroll To Project , About , Contact
 menuWrapper.addEventListener("click", function (e) {
@@ -18,36 +20,15 @@ menuWrapper.addEventListener("click", function (e) {
 headerScroll.addEventListener("click", function (e) {
   mainProject.scrollIntoView({ behavior: "smooth" });
 });
-
-// Transition
-function fadeInElementsOnScroll() {
-  elements.forEach((element) => {
-    const elementTop = element.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-    if (elementTop < windowHeight) {
-      setTimeout(() => {
-        element.style.opacity = 1;
-        element.style.transform = "translateX(0)";
-      }, 300);
-    } else {
-      element.style.opacity = 0;
-      element.style.transform = "translateX(-25rem)";
-    }
-  });
-}
-window.addEventListener("scroll", fadeInElementsOnScroll);
-
 // Copyed
 const copyButton = document.getElementById("copy-button");
 const email = "am.ranjbr.ir@gmail.com";
-
 copyButton.addEventListener("click", () => {
   navigator.clipboard.writeText(email);
   copyButton.insertAdjacentHTML(
     "afterend",
     '<p class="copy-message">Copied!</p>'
   );
-
   setTimeout(() => {
     const copyMessage = document.querySelector(".copy-message");
     copyMessage.remove();
@@ -58,6 +39,23 @@ function scrollToSection(sectionId) {
   const section = document.getElementById(sectionId);
   section.scrollIntoView({ behavior: "smooth" });
 }
+// Reveal section
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) entry.target.classList.add("section--hidden");
+  else entry.target.classList.remove("section--hidden");
+  // observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.2,
+  rootMargin: "-20px",
+});
+allSection.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
 // Scroll to top
 const calcScrollValue = () => {
   const scrollTop = document.querySelector(".scrollTop");
@@ -86,14 +84,11 @@ menuBtn.addEventListener("click", function () {
   menu.classList.toggle("active");
   menuBtn.classList.toggle("visibility");
 });
-
 // Close menu in click
-
 closeMenu.addEventListener("click", function () {
   menu.classList.remove("active");
   menuBtn.classList.toggle("visibility");
 });
-
 // Close menu without click
 document.addEventListener("mousedown", function (event) {
   if (!menu.contains(event.target) && !menuBtn.contains(event.target)) {
